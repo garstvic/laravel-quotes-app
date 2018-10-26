17e1a948bd95b0728976ebee6c3c28228860d6be
Victor Garstukov <garstvic@gmail.com>
2018-10-26 06:23:59 +0000

    Added pagination for quotes

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -14,12 +14,13 @@ class QuoteController extends Controller
             $quote_author = Author::where('name', $author)->first();
             
             if ($quote_author) {
-                $quotes = $quote_author->quotes()->orderBy('created_at', 'desc')->get();
+                //$quotes = $quote_author->quotes()->orderBy('created_at', 'desc')->get();
+                $quotes = $quote_author->quotes()->orderBy('created_at', 'desc')->paginate(6);
             }
         } else {
         
             //$quotes = Quote::all();
-            $quotes = Quote::orderBy('created_at', 'desc')->get();
+            $quotes = Quote::orderBy('created_at', 'desc')->paginate(6);
         }
         
         return view('index', ['quotes' => $quotes]);    
